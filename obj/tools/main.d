5f104cb41422a3301aad9306a584233283268adc
Amir MAHLA <amahla@e3r3p10.clusters.42paris.fr>
2022-12-21 16:25:30 +0100

    iterator vector done

--- obj/tools/main.d
+++
@@ -1,3 +0,0 @@
-obj/tools/main.o: srcs_tests/tools/main.cpp templates/metaprog.hpp
-
-templates/metaprog.hpp:
--- obj/vector/main.d
+++
@@ -1,6 +0,0 @@
-obj/vector/main.o: srcs_tests/vector/main.cpp templates/metaprog.hpp \
-  templates/random_iterator.hpp
-
-templates/metaprog.hpp:
-
-templates/random_iterator.hpp:
