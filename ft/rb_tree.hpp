c0c31853ad0f90eed3172194d65b730042726636
Amir MAHLA <amahla@e1r1p6.clusters.42paris.fr>
2023-01-06 12:36:29 +0100

    tree in progress

--- ft/rb_tree.hpp
+++ ft/rb_tree.hpp
@@ -6,38 +6,51 @@
 /*   By: amahla <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/05 13:29:41 by amahla            #+#    #+#             */
-/*   Updated: 2023/01/05 17:46:00 by amahla           ###   ########.fr       */
+/*   Updated: 2023/01/06 12:35:57 by amahla           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef __RB_TREE_HPP__
 # define __RB_TREE_HPP__
 
-namespace ft {
-
 	enum e_color { black = true, red = false };
 
+	template< typename T >
+	struct Node
+	{	
+		e_color			color;
+		node_pointer*	p;
+		node_pointer*	l;
+		node_pointer*	r;
+		T				content;
+	};
 
-	template< typename Key, typename T, typename _KeyOfValue,
+
+namespace ft {
+
+	template< typename Key, typename T, typename KeyFirst,
 		typename Compare, typename Allocator = std::allocator<T> >
  	class rb_tree {
 		
-		struct Node {	
-
-			typedef Node*		node_pointer;
-			typedef const Node*	const_node_pointer;
-	
-			e_color			color;
-			node_pointer*	p;
-			node_pointer*	l;
-			node_pointer*	r;
-			T				content;
-
-		};
-
 		public:
 
-			Node	*p;
+			typedef Key								key_type;
+			typedef T								value_type;
+			typedef T*								pointer;
+			typedef const T*						const_pointer;
+			typedef T&								reference;
+			typedef const T&						const_reference;
+			typedef Node<T>*						link_type;
+			typedef const Node<T>*					const_link_type;
+			typedef size_t							size_type;
+			typedef ptrdiff_t						difference_type;
+			typedef Allocator						allocator_type;
+
+		private:
+
+		Compare		_comp;
+		Node<T>		_root;
+		size_type	_sizeTree;
 
 
 
