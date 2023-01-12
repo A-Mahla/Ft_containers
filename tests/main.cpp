25b5985ff0677efc07d38fb26f0efde37b35cf3b
Amir MAHLA <amahla@e1r2p2.clusters.42paris.fr>
2023-01-12 16:09:45 +0100

    test 42 : OK

--- tests/main.cpp
+++ tests/main.cpp
@@ -27,6 +27,33 @@
 
 #define MAX_RAM 4294967296
 #define BUFFER_SIZE 4096
+struct Buffer
+{
+	int idx;
+	char buff[BUFFER_SIZE];
+};
+
+
+#define COUNT (MAX_RAM / (int)sizeof(Buffer))
+
+template<typename T>
+class MutantStack : public stack<T>
+{
+public:
+	MutantStack() {}
+	MutantStack(const MutantStack<T>& src) { *this = src; }
+	MutantStack<T>& operator=(const MutantStack<T>& rhs)
+	{
+		this->c = rhs.c;
+		return *this;
+	}
+	~MutantStack() {}
+
+	typedef typename stack<T>::container_type::iterator iterator;
+
+	iterator begin() { return this->c.begin(); }
+	iterator end() { return this->c.end(); }
+};
 
 struct Test {};
 
@@ -499,6 +526,72 @@ void	modifierMap( void )
 	#endif
 }
 
+/*=====================42 Test===============================*/
+
+void	test42( void )
+{
+	const int seed = 1000000;
+	srand(seed);
+
+	vector<std::string> vector_str;
+	vector<int> vector_int;
+	stack<int> stack_int;
+	vector<Buffer> vector_buffer;
+	stack<Buffer, std::deque<Buffer> > stack_deq_buffer;
+	map<int, int> map_int;
+
+	for (int i = 0; i < COUNT; i++)
+	{
+		vector_buffer.push_back(Buffer());
+	}
+
+	for (int i = 0; i < COUNT; i++)
+	{
+		const int idx = rand() % COUNT;
+		vector_buffer[idx].idx = 5;
+	}
+	vector<Buffer>().swap(vector_buffer);
+
+	try
+	{
+		for (int i = 0; i < COUNT; i++)
+		{
+			const int idx = rand() % COUNT;
+			vector_buffer.at(idx);
+			std::cerr << "Error: THIS VECTOR SHOULD BE EMPTY!!" <<std::endl;
+		}
+	}
+	catch(const std::exception& e)
+	{
+		//NORMAL ! :P
+	}
+	
+	for (int i = 0; i < COUNT; ++i)
+	{
+		map_int.insert(make_pair(rand(), rand()));
+	}
+
+	int sum = 0;
+	for (int i = 0; i < 10000; i++)
+	{
+		int access = rand();
+		sum += map_int[access];
+	}
+	std::cout << "should be constant with the same seed: " << sum << std::endl;
+
+	{
+		map<int, int> copy = map_int;
+	}
+	MutantStack<char> iterable_stack;
+	for (char letter = 'a'; letter <= 'z'; letter++)
+		iterable_stack.push(letter);
+	for (MutantStack<char>::iterator it = iterable_stack.begin(); it != iterable_stack.end(); it++)
+	{
+		std::cout << *it;
+	}
+	std::cout << std::endl;
+
+}
 
 // =================
 
@@ -509,7 +602,7 @@ int main(void)
 
 	// ==== VECTOR ====
 
-/*	constructVector();
+	constructVector();
 	beginAndEndVector();
 	capacityTestVector();
 	accessConstructVector();
@@ -527,11 +620,14 @@ int main(void)
 //	treeTest();
 
 	// ====================
-*/
+
 	constructMap();
 	accessMap();
 	capacityMap();
 	modifierMap();
+
+
+	test42();
 
 	clock_t end = clock();
 
